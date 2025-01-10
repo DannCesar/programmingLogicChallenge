840c5b844c711254520a6baa61e07999d8994b9e
Daniel Cesar <danielcesartds@gmail.com>
2025-01-10 15:47:26 -0300

    Feat: exercise three

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import faturamento from "./faturamento.json" assert { type: "json" };
+
 const INDICE = 13;
 
 function findValue(INDICE) {
@@ -31,5 +33,43 @@ function valueIsFibonacci(num) {
   }
   return unaffirmativeMessage;
 }
+//Solução do exercício numero 2
 //Digite o número como parâmetro da função para obter a resposta
 //console.log(valueIsFibonacci(1));
+
+function calculateBilling(faturamento) {
+  const billedDays = faturamento.filter((entry) => entry.valor > 0);
+  const values = faturamento.map((entry) => entry.valor);
+  const minValues = Math.min(...values);
+  const maxValues = Math.max(...values);
+
+  const totalBilledDays = values.reduce((entry, values) => entry + values, 0);
+  const monthlyAverage = totalBilledDays / values.length;
+  const daysWithHighestRevenue = billedDays.filter(
+    (entry) => entry.valor > monthlyAverage
+  ).length;
+  return {
+    minValues,
+    maxValues,
+    daysWithHighestRevenue,
+  };
+}
+
+const billingCalculationResult = calculateBilling(
+  faturamento.faturamentoMensal
+);
+const minValue = billingCalculationResult.minValues.toFixed(2);
+const maxValue = billingCalculationResult.maxValues.toFixed(2);
+const totalDaysWithHighestRevenue =
+  billingCalculationResult.daysWithHighestRevenue;
+
+//Solução do exercício numero 3
+// console.log(
+//   `O menor valor de faturamento ocorrido em um dia do mês foi de ${minValue} reais.`
+// );
+// console.log(
+//   `O maior valor de faturamento ocorrido em um dia do mês foi de ${maxValue} reais.`
+// );
+// console.log(
+//   `O numero de dias no mês em que o valor de faturamento diário foi superior à média mensal foi de ${totalDaysWithHighestRevenue} dias.`
+// );

f19806ec07e6da345e7930e1062902b35590f44f
Daniel Cesar <danielcesartds@gmail.com>
2025-01-10 13:23:54 -0300

    Feat: exercise two

--- src/index.js
+++ src/index.js
@@ -11,4 +11,25 @@ function findValue(INDICE) {
   return SOMA;
 }
 
-console.log("O valor final da variável SOMA é:", findValue(INDICE));
+//Resultado do exercício 1
+// console.log("O valor final da variável SOMA é:", findValue(INDICE));
+
+function valueIsFibonacci(num) {
+  let a = 0;
+  let b = 1;
+  const affirmativeMessage = `O número informado pertence à sequencia de Fibonnaci.`;
+  const unaffirmativeMessage = `O número informado não pertence à sequencia de Fibonnaci.`;
+  if (num === a || num === b) {
+    return affirmativeMessage;
+  } else if (b <= num) {
+    if (b === num) {
+      return affirmativeMessage;
+    }
+    let temp = b;
+    b = a + b;
+    a = temp;
+  }
+  return unaffirmativeMessage;
+}
+//Digite o número como parâmetro da função para obter a resposta
+//console.log(valueIsFibonacci(1));
